1ea2a28d0d2516bc2e837098c8aaefefb09f14c9
Nero671 <stas.kachalko@mail.ru>
2020-08-08 21:30:48 +0300

    chore: make consoles | script

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
-let money = 1000,
+let money = 10000,
     income = 'Фриланс',
     addExpenses = 'Интернет, такси, коммуналка',
     deposite = true,
     mission = 100000,
     period = 6;
+
+let budgetDay = money / 30;
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposite);
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + '$');
+console.log(addExpenses.toLowerCase().split(','));
+console.log(budgetDay);
