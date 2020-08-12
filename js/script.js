969fd1fbb266328654fa2a777dc1e10fc0331ecb
Nero671 <stas.kachalko@mail.ru>
2020-08-12 10:52:18 +0300

    chore: cleaned up the code | script

--- js/script.js
+++ js/script.js
@@ -15,41 +15,46 @@ let accumulatedMonth = getAccumulatedMonth();
 let budgetDay = accumulatedMonth / 30;
 let missionComplete = mission / accumulatedMonth;
 
+let showTypeOf = function(data) {
+  console.log(data, typeof(data));
+};
+
 
 function getExpensesMonth() {
   return amount1 + amount2;
-}
+};
 
 function getAccumulatedMonth() {
   return money - amount1 - amount2;
-}
+};
 
 function getTargetMonth() {
   return mission / accumulatedMonth;
-}
+};
 
-console.log(typeof money);
-console.log(typeof income);
-console.log(typeof deposite);
+let getStatusIncome = function () {
+  if (budgetDay >= 1200) {
+    return ('У вас высокий доход!')
+  } else if (budgetDay >= 600 && budgetDay <= 1200) {
+    return ('У вас средний доход!');
+  } else if (budgetDay <= 600) {
+    return ('К сожалению уровень доход ниже среднего');
+  } else if (budgetDay <= 0) {
+    return ('Что-то пошло не так...');
+  }
+};
+
+showTypeOf(money);
+showTypeOf(income);
+showTypeOf(deposite);
 console.log('Расходы за месяц: ' + getExpensesMonth());
 console.log('Возможные расходы: ' + addExpenses.split(','));
 console.log('Срок достижения цели: ' + getTargetMonth());
 console.log('Бюджет на день ' + Math.floor(budgetDay));
+console.log(getStatusIncome());
 
-// console.log('Период равен ' + period + ' месяцев');
-// console.log('Цель заработать ' + mission + '$');
-// console.log('Цель будет достигнута за ' + Math.ceil(missionComplete) + ' месяцев');
-// console.log('Бюджет на месяц ' + accumulatedMonth);
 
-// if (budgetDay >= 1200) {
-//   console.log('У вас высокий доход!')
-// } else if (budgetDay >= 600 && budgetDay <= 1200) {
-//   console.log('У вас средний доход!');
-// } else if (budgetDay <= 600) {
-//   console.log('К сожалению уровень доход ниже среднего');
-// } else if (budgetDay <= 0) {
-//   console.log('Что-то пошло не так...');
-// }
+
 
 
 
