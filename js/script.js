d385eb3a8249cda0cfdcbefe75c5752c77d1d24f
Nero671 <stas.kachalko@mail.ru>
2020-08-14 00:35:41 +0300

    chore: completed advanced task | script

--- js/script.js
+++ js/script.js
@@ -1,41 +1,74 @@
 'use strict';
 
+let isNumber = function (n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
 function quize() {
   let quizeNumber = 66;
+  let userAttempt = 10;
   console.log(quizeNumber);
+
+  let userGreeting = function (ask) {
+    if (ask) {
+      getNumber();
+    } else {
+      alert('Приходите еще!');
+    }
+  }
+
+  let attempts = function (attempt) {
+    attempt--;
+    return attempt;
+  }
+
+  let incorrectAnswer = function (userAnswer) {
+    userAttempt = attempts(userAttempt)
+    userGreeting(userAnswer);
+  }
   
   let getNumber = function() {
-
-    let userGreeting = function(ask) {
-      if(ask) {
-        getNumber();
-      } else {
-        alert('Приходите еще!')
-      }
-    }
 
   let userAnswer, writeNumber = +prompt('Угадай число от 1 до 100');
     
     if (writeNumber === null) {
       alert('Приходите еще!');
-    } else if (writeNumber > quizeNumber) {
-      userAnswer = confirm('Загаданное число меньше');
-      userGreeting(userAnswer);
-    } else if (writeNumber < quizeNumber) {
-      userAnswer = confirm('Загаданное число больше');
+    } else if (!isNumber(writeNumber) || (parseFloat(writeNumber) > 100)) {
+      userAnswer = confirm('Введите число от 0 до 100!');
       userGreeting(userAnswer);
     } else if (writeNumber === quizeNumber) {
-      alert('Вы выиграли!');
-    } else if (writeNumber !== 'number') {
-      userAnswer = confirm('Введите число!');
-      userGreeting(userAnswer);
-    } 
+      userAnswer = confirm('Поздравляю, Вы угадали!!!')
+      if (userAnswer) {
+        quizeNumber = 66;
+        userAttempt = 10;
+        game();
+      }
+    } else if (userAttempt > 1) {
+      if (writeNumber < quizeNumber) {
+        userAnswer = confirm('Загаданное число больше, осталось попыток ' + attempts(userAttempt));
+        incorrectAnswer(userAnswer);
+      } else if (writeNumber > quizeNumber) {
+        userAnswer = confirm('Загаданное число меньше, осталось попыток ' + attempts(userAttempt));
+        incorrectAnswer(userAnswer);
+      }
+    } else if (userAttempt === 1) {
+      userAnswer = confirm('Попытки закончились, хотите сыграть еще?');
+      if (userAnswer) {
+        quizeNumber = 66;
+        userAttempt = 10;
+        console.log("Загаданное число", quizeNumber);
+        game();
+      }
+    }
   }
   return getNumber;
 }
 
 let game = quize();
 game();
+
+
+
 
 
 
