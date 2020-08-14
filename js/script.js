42b3b8d1494d1786b6cb7e5367f85d25fc25c349
Nero671 <stas.kachalko@mail.ru>
2020-08-14 12:09:28 +0300

    fix: fix some bags | script

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 'use strict';
 
-let isNumber = function (n) {
+let isNumber = function(n) {
   return !isNaN(parseFloat(n)) && isFinite(n);
 }
 
 function quize() {
   let quizeNumber = Math.floor(Math.random() * 101);
   let userAttempt = 10;
-  console.log(quizeNumber);
+  console.log('Загаданное число: ' + quizeNumber);
 
-  let userGreeting = function (ask) {
+  let userGreeting = function(ask) {
     if (ask) {
       getNumber();
     } else {
@@ -22,25 +22,26 @@ function quize() {
     return attempt;
   }
 
-  let incorrectAnswer = function (userAnswer) {
-    userAttempt = attempts(userAttempt)
+  let incorrectAnswer = function(userAnswer) {
+    userAttempt = attempts(userAttempt);
     userGreeting(userAnswer);
   }
   
   let getNumber = function() {
 
-  let userAnswer, writeNumber = +prompt('Угадай число от 1 до 100');
+    let userAnswer, writeNumber = prompt('Угадай число от 1 до 100');
     
     if (writeNumber === null) {
       alert('Приходите еще!');
     } else if (!isNumber(writeNumber) || (parseFloat(writeNumber) > 100)) {
       userAnswer = confirm('Введите число от 0 до 100!');
       userGreeting(userAnswer);
-    } else if (writeNumber === quizeNumber) {
-      userAnswer = confirm('Поздравляю, Вы угадали!!!')
+    } else if (writeNumber == quizeNumber) {
+      userAnswer = confirm('Поздравляю, Вы угадали!');
       if (userAnswer) {
         quizeNumber = Math.floor(Math.random() * 101);
         userAttempt = 10;
+        console.log('Загаданное число: ' + quizeNumber);
         game();
       }
     } else if (userAttempt > 1) {
@@ -56,7 +57,7 @@ function quize() {
       if (userAnswer) {
         quizeNumber = Math.floor(Math.random() * 101);
         userAttempt = 10;
-        console.log("Загаданное число", quizeNumber);
+        console.log('Загаданное число: ' + quizeNumber);
         game();
       }
     }
