fe0ed081a150ee1e6375a57daa052be910d13f2d
Nero671 <stas.kachalko@mail.ru>
2020-08-18 19:37:39 +0300

    recover repository

--- js/script.js
+++ js/script.js
@@ -1,52 +1,23 @@
-'use strict';
+  'use strict'
 
-const calendar = () => {
+  let week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+  let day = new Date().getDay() - 1;
 
-
-let Data = new Date();
-let Year = Data.getFullYear();
-let Month = Data.getMonth();
-let Day = Data.getDay();
-let dayNumber = Data.getDate();
-let Hour = Data.getHours();
-let Minutes = Data.getMinutes();
-let Seconds = Data.getSeconds();
-
-let week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
-let month = ['Января', 'Февраля', 'Марта', 'Апреля', 'Мая', 'Июня', 'Июля', 'Августа', 'Сентября', 'Октября', 'Ноября', 'Декабря'];
-
-let currentDay = week[Day - 1];
-let currentMonth = month[Month];
-
-// let changeHours = function() {
-//   if(Hour === 1, 21, 00) {
-//     document.write(' час ');
-//   } else if(Hour === 2, 3, 4, 22, 23) {
-//     document.write(' часа ');
-//   } else {
-//     document.write(' часов ');
-//   }
-// }
-
-let addZero = elem => {
-  if (String(elem).length === 1) {
-    return '0' + elem;
-  } else {
-    return String(elem);
+  for (let i = 0; i < week.length; i++) {
+    if (i === day) {
+      if (week[i] == 'Суббота' || week[i] == 'Воскресенье') {
+        document.write(`<p><b><i>${week[i]}</i></b></p>`);
+      } else {
+        document.write(`<p><b>${week[i]}</b></p>`);
+      }
+    } else if (week[i] == 'Суббота' || week[i] == 'Воскресенье') {
+      document.write(`<p><b>${week[i]}</b></p>`);
+    } else {
+      document.write(`<p>${week[i]}</p>`);
+    }
   }
-};
 
-document.write('Сегодня ' + currentDay + ', ' + dayNumber 
-                + ' ' + currentMonth + ' ' + Year + ' года' 
-                + ', ' + Hour + ' часов ' + Minutes + ' минут ' 
-                + Seconds + ' секунд ');
-
-document.write(addZero(dayNumber) + '.' + addZero(Month) + '.' + addZero(Year) + ' - '
-            +addZero(Hour) + '.' + addZero(Minutes) + '.' + addZero(Seconds));
-
-}
-
-setInterval(calendar, 1000)
+  console.log(week);
 
 
 
