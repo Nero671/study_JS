2ee282dbfb6cb95490d94939e81978f9acca1e86
Nero671 <stas.kachalko@mail.ru>
2020-08-27 13:26:36 +0300

    chore: need help with position | script

--- js/script.js
+++ js/script.js
@@ -1,33 +1,58 @@
 'use strict';
 
-function DomElement(selector, height, width, bg, fontSize) {
-  this.selector = selector;
-  this.height = height;
-  this.width = width;
-  this.bg = bg;
-  this.fontSize = fontSize;
-}
+document.addEventListener('DOMContentLoaded', function () {
 
-DomElement.prototype.newElem = function() {
-  let elem;
-  if(this.selector[0] === '.') {
-    elem = document.createElement('div');
-    elem.className = this.selector.slice(1);
+    let px = 10;
+
+
+  function DomElement(selector, height, width, bg, fontSize, position) {
+    this.selector = selector;
+    this.height = height;
+    this.width = width;
+    this.bg = bg;
+    this.fontSize = fontSize;
+    this.position = position;
   }
-  if (this.selector[0] === '#') {
-    elem = document.createElement('div')
-    elem.className = this.selector.slice(1);
+
+
+  DomElement.prototype.newElem = function() {
+    let elem;
+    if(this.selector[0] === '.') {
+      elem = document.createElement('div');
+      elem.className = this.selector.slice(1);
+    }
+    if (this.selector[0] === '#') {
+      elem = document.createElement('div')
+      elem.className = this.selector.slice(1);
+    }
+    elem.style.cssText = `
+      height: ${this.height}px;
+      width: ${this.width}px;
+      background: ${this.bg};
+      font-size: ${this.fontSize}px;
+      position: ${this.position};
+    `;
+    return elem;
   }
-  elem.style.cssText = `
-    height: ${this.height}px;
-    width: ${this.width}px;
-    background: ${this.bg};
-    font-size: ${this.fontSize}px;
-  `;
-  return elem;
-}
 
-let elem1 = new DomElement('.block', 100, 100, 'red', 20);
+  let elem1 = new DomElement('.block', 100, 100, 'red', 20, 'absolute');
 
-document.body.appendChild(elem1.newElem());
 
+  document.body.appendChild(elem1.newElem());
+
+  document.addEventListener('keyup', event => {
+    let div = document.querySelector('div');
+    if(event.key === 'ArrowUp') {
+      div.style.top = px + 'px';
+    }
+    if (event.key === 'ArrowDown') {
+      div.style.down = px + 'px';
+    }
+    if (event.key === 'Arrowleft') {
+      div.style.left = px + 'px';
+    }
+    if (event.key === 'ArrowRight') {
+      div.style.right = px + 'px';
+    }
+  });
+});
