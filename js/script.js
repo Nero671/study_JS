c1205aaa515625945683504e9ff195e63dbabfcb
Nero671 <stas.kachalko@mail.ru>
2020-08-27 23:54:33 +0300

    fix

--- js/script.js
+++ js/script.js
@@ -1,15 +1,13 @@
 'use strict';
 
 class First {
+
   hello() {
     console.log('Привет, я метод родителя');
   }
 }
 
 class Second extends First {
-  constructor(hello) {
-    super(hello);
-  }
   hello() {
     super.hello();
     console.log('А я наследуемый метод!');
