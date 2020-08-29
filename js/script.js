7007ef520b94bff1a405104faee21b2defdff1af
Nero671 <stas.kachalko@mail.ru>
2020-08-29 17:27:54 +0300

    chore: practice reduce | script

--- js/script.js
+++ js/script.js
@@ -1,41 +1,11 @@
-const input1 = document.getElementById('a'),
-  input2 = document.getElementById('b'),
-  sum = document.getElementById('sum'),
-  mult = document.getElementById('mult'),
-  res = document.getElementById('res');
+function getResult(x, y) {
+  let result;
+  result = String(x ** y).split('');
+  result = result.reduce((previousValue, item) => +previousValue + +item, 0)
+  return result;
+}
 
-const calculator = {
-
-  sum: function () {
-    if (input1.value && input2.value) {
-      this.numA = +input1.value;
-      this.numB = +input2.value;
-      this.result = this.numA + this.numB
-    }
-  },
-  mult: function () {
-    if (input1.value && input2.value) {
-      this.numA = +input1.value;
-      this.numB = +input2.value;
-      this.result = this.numA * this.numB
-    }
-  },
-  show: function () {
-    if (input1.value && input2.value) {
-      res.value = this.result;
-    } 
-  }
-};
-
-sum.addEventListener('click', () => {
-  calculator.sum();
-  calculator.show();
-});
-
-mult.addEventListener('click', () => {
-  calculator.mult();
-  calculator.show();
-});
+console.log(getResult(4, 8));
 
 
 
