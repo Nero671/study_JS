be775e030434623d3055dd10a8f84743428a0ac4
Nero671 <stas.kachalko@mail.ru>
2020-08-29 17:55:37 +0300

    chore: completed task

--- js/script.js
+++ js/script.js
@@ -1,12 +1,35 @@
-function getResult(x, y) {
-  let result;
-  result = String(x ** y).split('');
-  result = result.reduce((previousValue, item) => +previousValue + +item, 0)
-  return result;
+const country = document.getElementById('country');
+const optionCountry = country.querySelectorAll('option');
+const city = document.getElementById('city');
+const result = document.querySelector('.result');
+
+
+const cityArr = {
+  rus: ['Москва', 'Санк-Петербург', 'Челябинск'],
+  uk:  ['Киев', 'Львов', 'Одесса'],
+  bel: ['Минск', 'Гродно', 'Брест'],
+  jap: ['Токио', 'Осака', 'Иокогама']
+};
+
+const chooseCity = function () {
+  city.style.display = 'inline-block';
+  city.textContent = '';
+  cityArr[country.value].forEach((item) => {
+    const option = document.createElement('option');
+    option.value = item;
+    option.textContent = item;
+    city.append(option);
+  }) 
 }
 
-console.log(getResult(4, 8));
+const showCity = () => {
+  const optionCity = city.querySelectorAll('option');
+  result.textContent = `${optionCountry[country.selectedIndex].textContent} ${optionCity[city.selectedIndex].textContent}`;
+}
 
+
+country.addEventListener('change', chooseCity);
+city.addEventListener('change', showCity);
 
 
 
