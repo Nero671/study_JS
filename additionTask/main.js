7cdeb705388e51a96f76abd2f224e4e9496e5f7b
Nero671 <stas.kachalko@mail.ru>
2020-09-02 23:25:15 +0300

    chore: create animation | script

--- additionTask/main.js
+++ additionTask/main.js
@@ -1,60 +1,43 @@
 'use strict';
 
-const calendar = () => {
+let count = 0,
+    start = document.querySelector('.start'),
+    stop = document.querySelector('.stop'),
+    block1 = document.querySelector('.block1'),
+    block2 = document.querySelector('.block2');
 
-  const text = document.querySelector('.text');
+let animate;
+let pause = false;
 
-  const timesOfDay = ['Доброе утро', 'Добрый день', 'Добрый вечер', 'Доброй ночи'];
-  const dayOfWeek =  ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
-  let date = new Date();
-  let newYear = new Date('1 january 2021');
-  let Year = date.getFullYear();
-  let Month = date.getMonth();
-  let Day = date.getDay();
-  let dayNumber = date.getDate();
-  let Hour = date.getHours();
-  let Minutes = date.getMinutes();
-  let Seconds = date.getSeconds();
-  let currentDay = dayOfWeek[Day];
-
-  const getTimesOfDay = function () {
-    if (Hour >= 6 && Hour <= 12) {
-      return timesOfDay[0];
-    } else if (Hour > 12 && Hour <= 18) {
-      return timesOfDay[1];
-    } else if (Hour > 18 && Hour <= 22) {
-      return timesOfDay[2];
-    } else if (Hour > 22 && Hour < 6) {
-      return timesOfDay[3];
-    }
-  };
-
-  const getTime = function() {
-    if(Hour >= 24 && Hour <= 12) {
-      return 'AM';
-    } else {
-      return 'PM';
-    }
+let flyAnimate = function() {
+  animate = requestAnimationFrame(flyAnimate);
+  count++;
+  if(count < 350) {
+    block1.style.top = count + 'px';
+    block2.style.left = count * 2 + 'px';
+  } else if(count < 500) {
+    block2.style.left = count * 2 + 'px';
+  } else if (count > 500) {
+    cancelAnimationFrame(animate);
   }
+};
 
-  const getNewYearsDays = function() {
-    const amountOfDays = Math.ceil((newYear - date) / 86400000);
-    if(amountOfDays % 10 === 1 && amountOfDays % 10 !== 11) {
-      return amountOfDays + ' день';
-    } else if(amountOfDays % 10 === 2 || amountOfDays % 10 === 3 || amountOfDays % 10 === 4) {
-      return amountOfDays + ' дня';
-    } else {
-      return amountOfDays + ' дней';
-    }
+
+start.addEventListener('click', () => {
+  if (!pause) {
+    animate = requestAnimationFrame(flyAnimate);
+    pause = true;
+  } else {
+    cancelAnimationFrame(animate);
+    pause = false;
   }
+});
 
-  text.innerHTML = `
-    ${getTimesOfDay()}, <br>
-    Cегодня: ${currentDay}, <br>
-    Текущее время: ${Hour}:${Minutes}:${Seconds} ${getTime()}, <br>
-    До нового года осталось: ${getNewYearsDays()}
-  `;
-
-}
-
-setInterval(calendar, 1000);
+stop.addEventListener('click', () => {
+  cancelAnimationFrame(animate);
+  count = 0;
+  block1.style.top = 0;
+  block1.style.left = 0;
+  block2.style.top = 0;
+  block2.style.left = 0;
+});
