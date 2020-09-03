84f6d07a14460f3f96aee25f03279312eff5e474
Nero671 <stas.kachalko@mail.ru>
2020-09-03 11:57:09 +0300

    chore: created popup animation | script

--- script/script.js
+++ script/script.js
@@ -44,7 +44,69 @@
 
 
   countTimer('12 september 2020');
-  // 
+  //Menu
+
+  const toggleMenu = () => {
+    const btnMenu = document.querySelector('.menu'),
+          menu = document.querySelector('menu'),
+          closeBtn = document.querySelector('.close-btn'),
+          menuItems = menu.querySelectorAll('ul>li');
+    
+    const handlerMenu = () => {
+      menu.classList.toggle('active-menu');
+    };
+
+    btnMenu.addEventListener('click', handlerMenu);
+    closeBtn.addEventListener('click', handlerMenu);
+    menuItems.forEach(elem => elem.addEventListener('click', handlerMenu));
+  };
+
+  toggleMenu();
+  //popup
+
+  const togglePopu = () => {
+    const popup = document.querySelector('.popup'),
+          popupBtn = document.querySelectorAll('.popup-btn'),
+          popupClose = document.querySelector('.popup-close'),
+          popupContent = document.querySelector('.popup-content');
+    
+    popupContent.style.left = '0';
+    let count = 0,
+        popupInterval;
+
+    const popupAnimate = () => {
+      count++;
+      popupInterval = requestAnimationFrame(popupAnimate);
+      popupContent.style.left = count + '%';
+      if (count === 40) {
+        cancelAnimationFrame(popupInterval);
+      }
+    }
+    
+    popupBtn.forEach(elem => elem.addEventListener('click', () => {
+      popup.style.display = 'block';
+      if (document.documentElement.clientWidth > 768) {
+        popupInterval = requestAnimationFrame(popupAnimate);
+      } else {
+        popupContent.removeAttribute('style');
+      }
+    }));
+    popupClose.addEventListener('click', () => {
+      popup.style.display = 'none';
+      count = 0;
+    });
+  };
+
+  togglePopu();
+
+
+
+
+
+
+
+
+
 
 
 
