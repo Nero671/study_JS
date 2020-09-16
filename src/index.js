8df453d17ede7f9008edd9dd8f8e3ffc3776ffd3
Nero671 <stas.kachalko@mail.ru>
2020-09-16 16:31:47 +0300

    chore: complete the project

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 'use strict';
 
+import "@babel/polyfill";
+import 'nodelist-foreach-polyfill';
+import elementClosest from 'element-closest';
+elementClosest(window);
+import 'formdata-polyfill';
+import 'es6-promise/auto';
+
 import countTimer from './modules/countTimer';
 import toggleMenu from './modules/toggleMenu';
 import togglePopu from './modules/togglePopu';
